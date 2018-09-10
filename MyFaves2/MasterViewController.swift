ebc5df0e5f515019f53d7d2dc79b5bb2f8e3bc9d
CalebKrahn <35338495+CalebKrahn@users.noreply.github.com>
2018-09-10 17:57:31 -0500

    CK Update

--- MyFaves2/MasterViewController.swift
+++ MyFaves2/MasterViewController.swift
@@ -37,10 +37,11 @@ class MasterViewController: UITableViewController {
         ListOfPhotos = [
             UIImage(named: "mall.png")!,
             UIImage(named: "bikes.jpg")!,
+            UIImage(named: "googles.png")!,
             UIImage(named: "googles.png")!]
 
-        ListOfFavs = ["Mall","Biking", "Google"]
-        FavDetails = ["http://www.shopcherryvalemall.com/","http://www.bicycling.com", "http://www.google.com"]
+        ListOfFavs = ["Cherryvale Mall","Bikes", "Google Search"]
+        FavDetails = ["http://www.msn.com/","http://www.rockvalleycollege.edu", "http://www.yahoo.com", "http://www.google.com"]
         
         if let split = self.splitViewController {
             let controllers = split.viewControllers
